6f300aeab383f9b3c0a53b0e8df8a9d722a23776
LiamNixon <liam_nixon@outlook.com>
2023-09-04 17:42:18 +1000

    Set up project
    TODO:
    Fix blog reroute logic
    Connect to contentful
    Create content type components
    Test!

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+// Import Packages
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Home Page</title>
+      </Helmet>
+      <p>TEST</p>
+    </>
+  );
+};
+
+export default Home;
